2eb28649b1e37b0d78e9a96975617b9188e61cad
Stern_Lee_04 <jayvadalia04@gmail.com>
2025-05-04 00:23:45 +0530

    connected components

--- Graph/ConnectedComponents.java
+++ Graph/ConnectedComponents.java
@@ -1,99 +1,46 @@
 import java.util.*;
+public class ConnectedComponents extends Graph {
 
-/**
- * ConnectedComponents
- * It is possible that threre is no completely connection is not exist.
- * 
- * ex. 0 - 1 - 2    4 - 5
- *         | /
- *         3
- * here 2 connected components in graph.
- * 
- * simply create visited outside and traverse to cover all vertices.
- */
-public class ConnectedComponents {
-    ArrayList<Edge>[] graph;
-
-    static public class Edge {
-
-        int src, dst, wgt;
-
-        public Edge(int src, int dst, int wgt) {
-            this.src = src;
-            this.dst = dst;
-            this.wgt = wgt;
+    public void componentsBfs() {
+        boolean[] visited = new boolean[getSize()];
+        for (int i = 0; i < visited.length; i++) {
+            bfs(i, visited);
         }
     }
 
-    void create(int v) {
-        this.graph = new ArrayList[v];
-        for (int i = 0; i < v; i++) {
-            this.graph[i] = new ArrayList<>();
+    public void componentsDfs() {
+        boolean[] visited = new boolean[getSize()];
+        for (int i = 0; i < visited.length; i++) {
+            dfs(i, visited);
         }
     }
-    void dfs() {
-        boolean[] visited = new boolean[graph.length];
-        for (int i = 0; i < graph.length; i++) {
-            if (!visited[i]) {
-                dfsutil(i, visited);
-            }
-        }
-    }
-    public void dfsutil(int curr, boolean[] visited) {
 
-        System.out.print(curr + " ");
-        visited[curr] = true;
-
-        for (int i = 0; i < graph[curr].size(); i++) {
-            Edge e = graph[curr].get(i);
-            if (!visited[e.dst]) {
-                dfsutil(e.dst, visited);
-            }
-        }
-    }
-    
-    void bfs() {
-        boolean[] visited = new boolean[graph.length];
-        for (int i = 0; i < graph.length; i++) {
-            if (!visited[i]) {
-                bfsutil(visited);
-            }
-        }
-    }
-    public void bfsutil(boolean[] visited) {
-        Queue<Integer> q = new LinkedList<>();
-
-        q.add(0);
-        visited[0] = true;
-        while (!q.isEmpty()) {
-            int curr = q.remove();
-            System.out.print(curr + " ");
-
-            for (Edge e : graph[curr]) {
-                if (!visited[e.dst]) {
-                    q.add(e.dst);
-                    visited[e.dst] = true;
-                }
-            }
-        }
-    }
     public static void main(String[] args) {
-        ConnectedComponents g = new ConnectedComponents();
-        g.create(6);
+        ConnectedComponents graph = new ConnectedComponents();
+        graph.addEdge(0, 1, 1);
+        graph.addEdge(0, 2, 1);
 
-        // component 1
-        g.graph[0].add(new Edge(0, 1, 0));
-        g.graph[1].add(new Edge(1, 0, 0));
-        g.graph[1].add(new Edge(1, 2, 0));
-        g.graph[1].add(new Edge(1, 3, 0));
-        g.graph[2].add(new Edge(2, 1, 0));
-        g.graph[2].add(new Edge(2, 3, 0));
-        g.graph[3].add(new Edge(3, 1, 0));
-        g.graph[3].add(new Edge(3, 2, 0));
-        // component 2
-        g.graph[4].add(new Edge(4,5,0));
-        g.graph[5].add(new Edge(5,4,0));
+        graph.addEdge(1, 0, 1);
+        graph.addEdge(1, 3, 3);
+
+        graph.addEdge(2, 0, 1);
+        graph.addEdge(2, 4, 0);
+
+        graph.addEdge(3, 1, 3);
+        graph.addEdge(3, 4, 1);
+        graph.addEdge(3, 5, 2);
+
+        graph.addEdge(4, 2, 0);
+        graph.addEdge(4, 3, 1);
+        graph.addEdge(4, 5, 1);
         
-        g.dfs();
+        graph.addEdge(5, 3, 2);
+        graph.addEdge(5, 4, 1);
+        graph.addEdge(5, 6, 1);
+
+        graph.addEdge(6, 5, 1);
+
+        // graph.componentsBfs();
+        graph.componentsDfs();
     }
 }
--- Graph/Graph.java
+++ Graph/Graph.java
@@ -1,19 +1,25 @@
 import java.util.*;
+
 public class Graph {
-    
+
     private ArrayList<ArrayList<Edge>> graph;
 
-    Graph() {
-        this.graph = new ArrayList<>();
-    }
-    
     public static class Edge {
         int src, dest, wgt;
+
         Edge(int src, int dest, int wgt) {
             this.src = src;
             this.dest = dest;
             this.wgt = wgt;
         }
+    }
+
+    Graph() {
+        this.graph = new ArrayList<>();
+    }
+
+    public int getSize() {
+        return graph.size();
     }
 
     void addEdge(int src, int dest, int wgt) {
@@ -23,10 +29,9 @@ public class Graph {
         }
         graph.get(src).add(new Edge(src, dest, wgt));
     }
-    
-    void bfs(int src) {
+
+    void bfs(int src, boolean[] visited) {
         Queue<Integer> q = new LinkedList<>();
-        boolean[] visited = new boolean[graph.size()];
 
         q.add(src);
         while (!q.isEmpty()) {
@@ -34,7 +39,7 @@ public class Graph {
             if (!visited[current]) {
                 System.out.println(current);
                 visited[current] = true;
-                for(int i = 0; i < graph.get(current).size() ; i++) {
+                for (int i = 0; i < graph.get(current).size(); i++) {
                     Edge e = graph.get(current).get(i);
                     if (!visited[e.dest]) {
                         q.add(e.dest);
@@ -45,44 +50,16 @@ public class Graph {
     }
 
     void dfs(int src, boolean[] visited) {
-        System.out.println(src);
-        visited[src] = true;
+        if (visited[src] == false) {
+            System.out.println(src);
+            visited[src] = true;
 
-        for (int i = 0; i < graph.get(src).size(); i++) {
-            Edge e = graph.get(src).get(i);
-            if (visited[e.dest] == false) {
-                dfs(e.dest, visited);
+            for (int i = 0; i < graph.get(src).size(); i++) {
+                Edge e = graph.get(src).get(i);
+                if (visited[e.dest] == false) {
+                    dfs(e.dest, visited);
+                }
             }
         }
-    }
-    
-
-    public static void main(String[] args) {
-        Graph graph = new Graph();
-        graph.addEdge(0, 1, 1);
-        graph.addEdge(0, 2, 1);
-
-        graph.addEdge(1, 0, 1);
-        graph.addEdge(1, 3, 3);
-
-        graph.addEdge(2, 0, 1);
-        graph.addEdge(2, 4, 0);
-
-        graph.addEdge(3, 1, 3);
-        graph.addEdge(3, 4, 1);
-        graph.addEdge(3, 5, 2);
-
-        graph.addEdge(4, 2, 0);
-        graph.addEdge(4, 3, 1);
-        graph.addEdge(4, 5, 1);
-        
-        graph.addEdge(5, 3, 2);
-        graph.addEdge(5, 4, 1);
-        graph.addEdge(5, 6, 1);
-
-        graph.addEdge(6, 5, 1);
-
-        graph.bfs(0);
-        graph.dfs(0, new boolean[graph.graph.size()]);
     }
 }
